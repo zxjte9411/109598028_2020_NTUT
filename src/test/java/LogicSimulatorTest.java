293a36ec879d804fb5a1970969685acafd2e4b71
zxjte9411 <a5852241@gmail.com>
2020-08-25 19:32:31 +0800

    通過測試

--- src/test/java/LogicSimulatorTest.java
+++ src/test/java/LogicSimulatorTest.java
@@ -18,26 +18,51 @@ public class LogicSimulatorTest
     }
 
     @Test
+    public void testLoad()
+    {
+        LogicSimulator logicSimulator = new LogicSimulator();
+
+        assertFalse(logicSimulator.load("FILE_NOT_EXIST"));
+        assertTrue(logicSimulator.load(file1Path));
+        assertEquals(3, logicSimulator.getInputPinsSize());
+        assertEquals(1, logicSimulator.getOutputPinsSize());
+        assertEquals(3, logicSimulator.getCircuitsSize());
+
+        assertTrue(logicSimulator.load(file2Path));
+        assertEquals(3, logicSimulator.getInputPinsSize());
+        assertEquals(2,
+                logicSimulator.getOutputPinsSize());
+        assertEquals(5, logicSimulator.getCircuitsSize());
+    }
+
+    @Test
     public void testGetSimulationResult()
     {
         LogicSimulator logicSimulator = new LogicSimulator();
-        try {
-            logicSimulator.load(file1Path);
-        }
-        catch (Exception e)
-        {
-            System.out.println(e.toString());
-        }
+
+        logicSimulator.load(file1Path);
 
         Vector<Boolean> inputValues = new Vector<>();
         inputValues.add(false);
         inputValues.add(true);
         inputValues.add(true);
+
         assertEquals("Simulation Result:\n" +
                 "i i i | o\n" +
                 "1 2 3 | 1\n" +
                 "------+--\n" +
                 "0 1 1 | 0\n", logicSimulator.getSimulationResult(inputValues));
+
+        inputValues = new Vector<>();
+        inputValues.add(true);
+        inputValues.add(false);
+        inputValues.add(false);
+
+        assertEquals("Simulation Result:\n" +
+                "i i i | o\n" +
+                "1 2 3 | 1\n" +
+                "------+--\n" +
+                "1 0 0 | 1\n", logicSimulator.getSimulationResult(inputValues));
     }
 
     @Test
@@ -46,35 +71,34 @@ public class LogicSimulatorTest
         LogicSimulator logicSimulator = new LogicSimulator();
 
         logicSimulator.load(file1Path);
-        logicSimulator.getTruthTable();
-//        assertEquals("Truth table:\n" +
-//                "i i i | o\n" +
-//                "1 2 3 | 1\n" +
-//                "------+--\n" +
-//                "0 0 0 | 0\n" +
-//                "0 0 1 | 0\n" +
-//                "0 1 0 | 0\n" +
-//                "0 1 1 | 0\n" +
-//                "1 0 0 | 1\n" +
-//                "1 0 1 | 1\n" +
-//                "1 1 0 | 0\n" +
-//                "1 1 1 | 0\n", logicSimulator.getTruthTable());
+        assertEquals("Truth table:\n" +
+                "i i i | o\n" +
+                "1 2 3 | 1\n" +
+                "------+--\n" +
+                "0 0 0 | 0\n" +
+                "0 0 1 | 0\n" +
+                "0 1 0 | 0\n" +
+                "0 1 1 | 0\n" +
+                "1 0 0 | 1\n" +
+                "1 0 1 | 1\n" +
+                "1 1 0 | 0\n" +
+                "1 1 1 | 0\n", logicSimulator.getTruthTable());
 
         logicSimulator = new LogicSimulator();
 
         logicSimulator.load(file2Path);
 
-//        assertEquals("Truth table:\n" +
-//                "i i i | o o\n" +
-//                "1 2 3 | 1 2\n" +
-//                "------+----\n" +
-//                "0 0 0 | 0 1\n" +
-//                "0 0 1 | 0 1\n" +
-//                "0 1 0 | 0 1\n" +
-//                "0 1 1 | 0 1\n" +
-//                "1 0 0 | 1 0\n" +
-//                "1 0 1 | 1 0\n" +
-//                "1 1 0 | 0 1\n" +
-//                "1 1 1 | 0 1\n", logicSimulator.getTruthTable());
+        assertEquals("Truth table:\n" +
+                "i i i | o o\n" +
+                "1 2 3 | 1 2\n" +
+                "------+----\n" +
+                "0 0 0 | 0 1\n" +
+                "0 0 1 | 0 1\n" +
+                "0 1 0 | 0 1\n" +
+                "0 1 1 | 0 1\n" +
+                "1 0 0 | 1 0\n" +
+                "1 0 1 | 1 0\n" +
+                "1 1 0 | 0 1\n" +
+                "1 1 1 | 0 1\n", logicSimulator.getTruthTable());
     }
 }
